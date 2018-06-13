5767a5f4ee9b1add5889953d3f7ea9561e255e8c
danish123gupta <gupta.danish236@gmail.com>
2018-06-13 19:39:37 +0530

    first commit

---
+++ assignment.py
@@ -0,0 +1,33 @@
+#Q.NO.1
+
+print('hello world')
+
+#Q.no.2
+
+print("Acad"+"view")
+
+#Q.NO.3
+
+x=input('enter the value of x :')
+y= input('enter the value of y:')
+z=input('enter the value of z:')
+print(x,y,z)
+
+
+
+#Q.NO.4
+print("Let\'s get started")
+
+#Q.NO.5
+
+
+s="Acadview"
+course="Python"
+fees= 5000
+print('%s %s %d '%(s, course,fees))
+
+
+#Q.NO.6
+name="Tony Stark"
+salary=1000000
+print('%s'' %d' %(name,salary))
